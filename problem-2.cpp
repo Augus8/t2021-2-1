78ff7ff3d8790db4f754d463d9a3149b5518cdc2
Augus8 <87463394+Augus8@users.noreply.github.com>
2021-07-15 04:12:16 -0700

    Add files via upload

---
+++ problem-2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+   
+  int a,b;
+
+  int output=1;
+  
+  cout<<"\n a=";
+  
+  cin>>a;
+if(a==0)
+{
+ exit;
+}
+else
+{ 
+b=a%2;
+  
+if(b==0)
+    
+{
+    
+  a=a-1;
+ 
+}
+  
+  for(int i;i<a;i++)
+    
+{
+     
+  cout<<output;
+   
+  output=output+2;
+ 
+}
+    
+ 
+  return 0;
+
+}
+}
